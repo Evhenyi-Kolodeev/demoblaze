0ed903be898b3e04d25c687ac6a977bf6ee941d7
Ievgenii <e.kolodeev@gmail.com>
2023-02-09 09:13:46 +0100

    demoblaze v1 login

---
+++ cypress/e2e/demoblaze.cy.js
@@ -0,0 +1,39 @@
+describe('demoblaze', () => {
+  let user;
+  let randomIndex = Math.floor(Math.random() * 10000);
+  let username = `newusername${randomIndex}`;
+  let password = `strongPass${randomIndex}`;
+
+  
+
+  it('should allow register new user', () => {
+
+    cy.visit('/');
+    cy.get('#signin2')
+      .click();
+    cy.wait(1000);
+    cy.get('#sign-username')
+      .type(username);
+    cy.get('#sign-password')
+      .type(password);
+    cy.contains('button', 'Sign up')
+      .click();
+    cy.visit('/');
+    cy.get('#login2')
+      .click();
+    cy.wait(1000);
+    cy.get('#loginusername')
+      .type(username);
+    cy.get('#loginpassword')
+      .type(password);
+    cy.contains('button', 'Log in')
+      .click();
+    cy.contains('.hrefch', 'Samsung galaxy s6')
+      .click();
+    cy.get('.col-sm-12 > .btn')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Product added.`);
+    })
+  })
+})
